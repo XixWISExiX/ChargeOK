d77c5cec3d77a3ba33e87f4dd9cf131b63c807c7
XixWISExiX <addalooptoit@gmail.com>
2024-10-13 15:03:26 -0500

    ev-charges from database displayed to frontend

---
+++ charge-ok/netlify-express/src/README.js
@@ -0,0 +1,6 @@
+// If you are reading this file, you are probebly wondering why this is here. It's because netlify requires
+// conent in two folders, them being functions and src. Backend was moved into functions folder to avoid webpack
+// because it just breaks the application and is not needed for backend. However a js file needs to be present in src
+// even if it does nothing, hence this file.
+
+// DO NOT DELETE
